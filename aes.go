ea12d9aafb4a214f5b8ea77b25597ce1f3f17dbe
Joe <joe@xxtime.com>
2020-07-15 19:39:32 +0800

    AES

---
+++ aes.go
@@ -0,0 +1,68 @@
+package libsgo
+
+import (
+	"io"
+	"bytes"
+	"errors"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+)
+
+/**
+ * @docs https://golang.org/src/crypto/cipher/example_test.go
+ * @docs https://blog.csdn.net/whatday/article/details/98292648
+ * @docs https://segmentfault.com/a/1190000021267253
+ * @docs http://www.361way.com/golang-rsa-aes/5828.html
+ */
+
+func AesEncryptCBC(key []byte, plaintext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext = PKCS7Padding(plaintext, aes.BlockSize)
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	return ciphertext, nil
+}
+
+func AesDecryptCBC(key []byte, ciphertext []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext) // CryptBlocks can work in-place if the two arguments are the same.
+
+	return ciphertext, nil
+}
+
+func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
